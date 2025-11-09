539d78b319ef535bbbb364740b9cbb7ced8132b8
maribel-del <maribel@zocalo10.com>
2025-11-08 18:50:17 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+let mySprite: Sprite = null
+mySprite.x += 0
+animation.runMovementAnimation(
+mySprite,
+animation.animationPresets(animation.flyToCenter),
+2000,
+true
+)
+game.onUpdate(function () {
+    scene.cameraFollowSprite(mySprite)
+})
